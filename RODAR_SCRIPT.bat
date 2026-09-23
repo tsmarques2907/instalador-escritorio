8ee62bc04389b2b58a8e530819796a34fa95082d
Thiago Marques <tsmarques2907@gmail.com>
2026-09-23 12:30:48 -0300

    Update script to run instalador_marques.ps1

--- RODAR_SCRIPT.bat
+++ RODAR_SCRIPT.bat
@@ -1,4 +1,4 @@
 @echo off
 title Abrindo Instalador...
 :: Executa o PowerShell como Administrador e passa o script .ps1 usando -File sem conflito de aspas
-powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList '-ExecutionPolicy Bypass -NoExit -File """%~dp0instalador_fea.ps1"""'"
+powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList '-ExecutionPolicy Bypass -NoExit -File """%~dp0instalador_marques.ps1"""'"
